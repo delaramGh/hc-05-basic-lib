407743f056787819b35a9af972d6f65c71fa5e6d
DelaramGh <deli.zizou@gmail.com>
2024-05-01 17:11:39 -0400

    few functions added, uart setting still buggy!

--- Core/Inc/my_hc05_lib.h
+++ Core/Inc/my_hc05_lib.h
@@ -7,12 +7,21 @@
 
 
 #define HC05_UARTh  (&huart2)
+#define HC05_EN_PIN  GPIO_PIN_11
+#define HC05_EN_PORT GPIOB
 
 
+void HC05_ClearData(void);
+
+void HC05_test(char* s);
 
 HAL_StatusTypeDef HC05_IsDeviceReady(uint16_t timeout);
 
-void HC05_ClearData(void);
+HAL_StatusTypeDef HC05_Reset(uint16_t timeout);
+
+//HAL_StatusTypeDef HC05_uartSetting(char* parameters, uint16_t timeout);
+
+
 
 #endif
 
--- Core/Src/my_hc05_lib.c
+++ Core/Src/my_hc05_lib.c
@@ -4,10 +4,21 @@
 uint8_t rData_1bit[1], rData[50];
 
 
+void HC05_test(char* s)
+{
+	HC05_ClearData();
+	HAL_UART_Receive_IT(HC05_UARTh, rData_1bit, 1);
+	HAL_UART_Transmit(HC05_UARTh, (uint8_t*)s, strlen(s), 200);
+}
+
 
 
 HAL_StatusTypeDef HC05_IsDeviceReady(uint16_t timeout)
 {
+	HC05_ClearData();
+	HAL_GPIO_WritePin(HC05_EN_PORT, HC05_EN_PIN, GPIO_PIN_SET);
+	HAL_Delay(50);
+	
 	HAL_UART_Receive_IT(HC05_UARTh, rData_1bit, 1);
 	HAL_UART_Transmit(HC05_UARTh, (uint8_t*)"AT\r\n", strlen("AT\r\n"), 200);
 	
@@ -18,6 +29,44 @@ HAL_StatusTypeDef HC05_IsDeviceReady(uint16_t timeout)
 	
 	return HAL_OK;
 }
+
+
+
+HAL_StatusTypeDef HC05_uartSetting(char* parameters, uint16_t timeout)
+{
+	HC05_ClearData();
+	HAL_UART_Receive_IT(HC05_UARTh, rData_1bit, 1);
+	char buffer[40];
+	sprintf(buffer, "AT+UART%s\r\n", parameters);
+	HAL_UART_Transmit(HC05_UARTh, (uint8_t*)buffer, strlen(buffer), 200);
+	
+	uint32_t Tickstart = HAL_GetTick();
+	
+	while(strstr((char*)rData, "OK") == 0)
+		if((HAL_GetTick() - Tickstart) > timeout) return HAL_TIMEOUT;
+	
+	return HAL_OK;
+}
+
+
+HAL_StatusTypeDef HC05_Reset(uint16_t timeout)
+{
+	HC05_ClearData();
+	HAL_UART_Receive_IT(HC05_UARTh, rData_1bit, 1);
+	HAL_UART_Transmit(HC05_UARTh, (uint8_t*)"AT+RESET\r\n", strlen("AT+RESET\r\n"), 200);
+	
+	uint32_t Tickstart = HAL_GetTick();
+	
+	while(strstr((char*)rData, "OK") == 0)
+		if((HAL_GetTick() - Tickstart) > timeout) return HAL_TIMEOUT;
+	
+	HAL_GPIO_WritePin(HC05_EN_PORT, HC05_EN_PIN, GPIO_PIN_RESET);
+	HAL_Delay(10);
+	
+	return HAL_OK;
+}
+
+
 
 
 void HC05_ClearData(void)
